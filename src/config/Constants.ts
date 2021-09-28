e3875f6587f3126fcf68e103cd5cb2a8a79b3512
Alini Canedo <alini.canedo@gmail.com>
2021-09-27 21:34:43 -0300

    gera o encurtador e salva no banco

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+  API_URL: 'http://localhost:5000',
+  MONGO_CONNECTION:
+    'mongodb+srv://alini:123123123@encurtador-url.mxusc.mongodb.net/encurtador-url?retryWrites=true&w=majority'
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
-import express, { NextFunction, Response, Request } from 'express'
+import { URLController } from './controller/URLController'
+import express from 'express'
+import { MongoConnection } from 'database/model/MongoConnection'
 
-const api = express();
-api.get('/test', (req: Request, res: Response, next: NextFunction) => {
-  res.json({ sucess: 'ok' })
-})
-api.listen(5000, () => console.log('listening on port 5000'));
+
+const api = express()
+api.use(express.json())
+
+const database = new MongoConnection()
+database.connect()
+
+const urlController = new URLController()
+api.post('/shorten', urlController.shorten)
+api.get('/:hash', urlController.redirect)
+
+api.listen(5000, () => console.log('Express listening'))
